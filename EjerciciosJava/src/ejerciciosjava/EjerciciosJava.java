234f2a4e4711b7d1179bfd7bf9d1cab897e9ee0d
Ulises <Ulises@PC_Uli>
2015-10-06 20:11:45 +0200

    Subo primer ejemplo en Java

---
+++ EjerciciosJava/src/ejerciciosjava/EjerciciosJava.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosjava;
+
+/**
+ *
+ * @author Ulises
+ */
+public class EjerciciosJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int c;
+        int a=3+4;
+        int b=4-8;
+        c=a+b;
+        
+        // TODO code application logic here
+    }
+    
+}
